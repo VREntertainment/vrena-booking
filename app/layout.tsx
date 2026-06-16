d4686cf319e2e68b3747cb73429c3744733b93b8
VREntertainment <emile@vre-vietnam.com>
2026-06-16 20:09:51 +0700

    Update app favicon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "VRena-Booking-App",
+  title: "VRena | Book & Play",
   description: "Book and play VR games at VRena",
 };
 
